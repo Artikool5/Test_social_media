38e05b6961f13be9d12a76dd659f5438610b2944
artikool <artikool5@yandex.ru>
2022-12-26 21:39:27 +0300

    Подключил API пользователей, добавил дефолтный аватар, axios

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,47 +3,46 @@ const SET_USERS = "SET-USERS";
 
 let initState = {
   users: [
-    {
-      id: 1,
-      followed: true,
-      fullName: "Andrew",
-      status: "Looking for a job right now",
-      location: { country: "Russia", city: "Moscow" },
-      avatarURL:
-        "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
-    },
-    {
-      id: 2,
-      followed: false,
-      fullName: "Danil",
-      status: "Love my wife",
-      location: { country: "Ukraine", city: "Kiev" },
-      avatarURL:
-        "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
-    },
-    {
-      id: 3,
-      followed: false,
-      fullName: "Alex",
-      status: "Searching for love",
-      location: { country: "Russia", city: "Moscow" },
-      avatarURL:
-        "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
-    },
-    {
-      id: 4,
-      followed: true,
-      fullName: "Anton",
-      status: "Looking for a job right now",
-      location: { country: "Russia", city: "Rostov" },
-      avatarURL:
-        "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
-    },
+    // {
+    //   id: 1,
+    //   followed: true,
+    //   fullName: "Andrew",
+    //   status: "Looking for a job right now",
+    //   location: { country: "Russia", city: "Moscow" },
+    //   avatarURL:
+    //     "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
+    // },
+    // {
+    //   id: 2,
+    //   followed: false,
+    //   fullName: "Danil",
+    //   status: "Love my wife",
+    //   location: { country: "Ukraine", city: "Kiev" },
+    //   avatarURL:
+    //     "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
+    // },
+    // {
+    //   id: 3,
+    //   followed: false,
+    //   fullName: "Alex",
+    //   status: "Searching for love",
+    //   location: { country: "Russia", city: "Moscow" },
+    //   avatarURL:
+    //     "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
+    // },
+    // {
+    //   id: 4,
+    //   followed: true,
+    //   fullName: "Anton",
+    //   status: "Looking for a job right now",
+    //   location: { country: "Russia", city: "Rostov" },
+    //   avatarURL:
+    //     "https://sualoo.com/uploads/testimonial_images/1533277299_main-user.png",
+    // },
   ],
 };
 
 const usersReducer = (state = initState, action) => {
-  debugger;
   switch (action.type) {
     case TOGGLE_FOLLOWED_STATUS: {
       return {
@@ -56,7 +55,7 @@ const usersReducer = (state = initState, action) => {
       };
     }
     case SET_USERS: {
-      return { ...state, users: [...state.users, ...action.users] };
+      return { ...state, users: action.users };
     }
     default:
       return state;
